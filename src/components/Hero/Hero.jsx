bbb6a6b1e2ff414b0ad1f84194312d9a4655291f
vasylkostiuk <Vas@harmonydomes.com>
2022-05-11 19:36:01 +0300

    add-styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,15 +5,17 @@ const Hero = () => {
     return (
         <div className={styles.hero}>
             <div className={styles.imageContainer}>
-                <Image src='/image8.png' layout='fill'/>
+                <Image src='/image8.png' layout='fill' alt="Harmony Domes"/>
             </div>
             <div className='container'>
                 <div className={styles.ctaContainer}>
                     <div className={styles.linkContainer}>
-                        <a href="#">more</a>
+                        <a href="#" className={styles.link}>More</a>
                     </div>
-                    <h1>Geodomes</h1>
-                    <p>Harmony Domes will create the ideal space for you to live in harmony </p>
+                    <h1 className={styles.title}>Geodomes</h1>
+                    <p className={styles.description}>
+                        Harmony Domes will create the ideal space for you to live in harmony
+                    </p>
                 </div>
             </div>
         </div>
