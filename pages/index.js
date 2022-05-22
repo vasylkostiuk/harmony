b88e8354fd88354683180d1182ce2e0b57ee8d1d
vasylkostiuk <Vas@harmonydomes.com>
2022-05-22 11:51:53 +0300

    clear-index

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import {useQuery} from "@apollo/client";
-import {GET_ARTICLES} from "../src/apolloClient/queries";
-import {client} from "../src/apolloClient/client";
+import HeroSlider from "../src/components/Hero/HeroSlider";
+import {GetStaticMainPage} from "../src/preRender/ISR";
 
-export default function Home() {
-  const {data} = useQuery(GET_ARTICLES, {fetchPolicy: "no-cache"});
-  const res = client.readQuery({
-    query: GET_ARTICLES,
-  });
-  console.log(res, data)
-  return <></>
+export default function Home({heroBlocks, video}) {
+  return <HeroSlider heroBlocks={heroBlocks} video={video}/>
 }
+
+export const getStaticProps = GetStaticMainPage;
 
