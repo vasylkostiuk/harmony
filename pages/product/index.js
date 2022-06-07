6a8195f7ebffb47e7d85406bfcd72079ebd8a39e
vasylkostiuk <Vas@harmonydomes.com>
2022-06-07 19:41:15 +0300

    fix-change-page-filtering

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,10 +1,23 @@
 import {GetStaticProducts} from "../../src/preRender/ISR";
 import LayoutProductContainer from "../../src/components/LayoutProduct/LayoutProductContainer";
-import {productsVar, currentProductsVar} from "../../src/apolloClient/reactiveVariables/products";
+import {
+    productsVar,
+    currentProductsVar,
+    currentCategoryVar,
+    currentFilterVar
+} from "../../src/apolloClient/reactiveVariables/products";
 
 const Products = ({products}) => {
     productsVar([...products]);
     currentProductsVar([...products]);
+    currentCategoryVar('All products');
+    currentFilterVar(
+        {
+            showDropdown: false,
+            currentFilter: 'New products'
+        }
+    );
+
     return (
         <div className="container">
            <LayoutProductContainer/>
