fcc9dcf674c2b8b1325cef3e402822d35da2c523
vasylkostiuk <Vas@harmonydomes.com>
2022-08-29 21:23:00 +0300

    add-header-click-outside

--- src/components/global/HeaderBody/HeaderBody.jsx
+++ src/components/global/HeaderBody/HeaderBody.jsx
@@ -1,12 +1,30 @@
 import styles from './HeaderBody.module.css';
 import {useReactiveVar} from "@apollo/client";
 import {showHeaderDropdown} from "../../../apolloClient/reactiveVariables/header";
+import {useEffect, useRef} from "react";
 
 const HeaderBody = ({children}) => {
     const isOpened = useReactiveVar(showHeaderDropdown);
+    const wrapperRef = useRef(null);
+
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+            function handleClickOutside(event) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    showHeaderDropdown(false);
+                }
+            }
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
+    useOutsideAlerter(wrapperRef);
 
     return (
-        <div className={`${styles.container} ${isOpened ? styles.opened : ''}`}>
+        <div className={`${styles.container} ${isOpened ? styles.opened : ''}`} ref={wrapperRef}>
             {children}
         </div>
     );
