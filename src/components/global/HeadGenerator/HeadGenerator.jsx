1f86cd983662edbf384da90fcdb23f981a3b8c02
vasylkostiuk <Vas@harmonydomes.com>
2023-01-25 18:27:51 +0200

    add-privacy-policy

--- src/components/global/HeadGenerator/HeadGenerator.jsx
+++ src/components/global/HeadGenerator/HeadGenerator.jsx
@@ -4,6 +4,7 @@ const HeadGenerator = ({ title, description, keywords }) => {
   return (
       <Head>
           <title>{title}</title>
+          <meta property="og:title" content={title} key="title" />
           <meta property="og:description" content={description} key="description" />
           <meta property="og:keywords" name="keywords" content={keywords} key="keywords" />
       </Head>
