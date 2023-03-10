8eb697551990656aac62a8df091c69631b44ead0
Nick Kleymenov <nick@kleymenov.pro>
2023-03-10 17:50:23 +0300

    Added compression options to the image URLs

--- src/components/Sequence/atoms/SequenceInfo/SequenceInfo.jsx
+++ src/components/Sequence/atoms/SequenceInfo/SequenceInfo.jsx
@@ -11,21 +11,25 @@ const SequenceInfo = ({sequenceInfo}) => {
         thirdImage
     } = sequenceInfo;
 
+    firstImage.compressedUrl = `${firstImage?.url}?resize=width:${firstImage?.width},fit:crop/quality=value:75/output=format:webp/compress`;
+    secondImage.compressedUrl = `${secondImage?.url}?resize=width:${secondImage?.width},fit:crop/quality=value:75/output=format:webp/compress`;
+    thirdImage.compressedUrl = `${thirdImage?.url}?resize=width:${thirdImage?.width},fit:crop/quality=value:75/output=format:webp/compress`;
+
     return (
         <div className="container">
             <div className={styles.container}>
                 <div className={styles.first}>
                     <h3 className={styles.title}>{title}</h3>
                     <p className={`${styles.text} ${styles.text__first}`} dangerouslySetInnerHTML={{__html: firstText}}></p>
-                    <img className={`${styles.image} ${styles.image__first}`} src={firstImage?.url} alt="Harmony Domes"/>
+                    <img className={`${styles.image} ${styles.image__first}`} src={firstImage?.compressedUrl} alt="Harmony Domes"/>
                 </div>
                 <div className={styles.second}>
                     <p className={`${styles.text} ${styles.text__second}`} dangerouslySetInnerHTML={{__html: secondText}}></p>
-                    <img className={`${styles.image} ${styles.image__second}`} src={secondImage?.url} alt="Harmony Domes"/>
+                    <img className={`${styles.image} ${styles.image__second}`} src={secondImage?.compressedUrl} alt="Harmony Domes"/>
                 </div>
                 <div className={styles.third}>
                     <p className={`${styles.text} ${styles.text__third}`} dangerouslySetInnerHTML={{__html: thirdText}}></p>
-                    <img className={`${styles.image} ${styles.image__third}`} src={thirdImage?.url} alt="Harmony Domes"/>
+                    <img className={`${styles.image} ${styles.image__third}`} src={thirdImage?.compressedUrl} alt="Harmony Domes"/>
                 </div>
             </div>
         </div>
