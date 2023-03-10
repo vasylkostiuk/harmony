73d986ace9c496441a12cb313905ff9d212cfe52
Nick Kleymenov <nick@kleymenov.pro>
2023-03-10 17:26:18 +0300

    Custom loader tweaks - added automated transformations to images (quality = 75, webp format)

--- src/preRender/customLoader.js
+++ src/preRender/customLoader.js
@@ -1,7 +1,7 @@
 const customLoader = ({src, width}) => {
     const relativeSrc = (src) => src.split('/').pop();
 
-    return `https://media.graphassets.com/resize=width:${width}/${relativeSrc(
+    return `https://media.graphassets.com/resize=width:${width},fit:crop/quality=value:75/output=format:webp/compress/${relativeSrc(
         src
     )}`;
 }
