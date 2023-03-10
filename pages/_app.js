7464c61df2296d782be5ca8ce021bbe157e6e686
Nick Kleymenov <nick@kleymenov.pro>
2023-03-10 10:23:48 +0300

    Added Google Tag Manager code

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
 import '../styles/globals.css';
 import {ApolloProvider} from "@apollo/client";
 import {client} from "../src/apolloClient/client";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
+      <>
+          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXX"></Script>
+          <Script
+              id='google-analytics'
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                  __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'AW-406986847', {
+                        page_path: window.location.pathname,
+                        });
+                        gtag('event', 'conversion', {'send_to': 'AW-406986847/hN6sCKy2zfoBEN_AiMIB'});
+                        console.log('Google Tag Manager has been loaded.');
+                        `,
+              }}
+          ></Script>
       <ApolloProvider client={client}>
           <Component {...pageProps} />
       </ApolloProvider>
+      </>
   );
 }
 
